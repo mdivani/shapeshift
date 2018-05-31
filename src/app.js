3f22e5ef528d8eab0df6618a6bb7baa794da60ef
mdivani <georgemdivani.gm@gmail.com>
2018-05-31 13:02:17 +0400

    configure redux

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
+import HomePage from './layout/HomePage';
 
-
+const store = configureStore();
 const jsx = (
-    <div>
-        <h1>BTCJP Shapeshift</h1>
-    </div>
+    <Provider store={store}>
+        <HomePage />
+    </Provider>
 )
 
 ReactDOM.render(jsx, document.getElementById('root'));
