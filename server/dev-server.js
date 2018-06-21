94c51443575645c2d452c22c3021c19dedcf943c
mdivani <georgemdivani.gm@gmail.com>
2018-06-21 21:12:18 +0400

    server formating

--- server/dev-server.js
+++ server/dev-server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
