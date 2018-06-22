d5935c02cb73893f2b2652f80b366b19e15c23bf
mdivani <georgemdivani.gm@gmail.com>
2018-06-22 08:24:39 +0400

    webpack remove path to dist

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
     return {
       entry: './src/app.js',
       output: {
-        path: path.join(__dirname, 'public', 'dist'),
+        path: path.join(__dirname, 'public'),
         filename: 'bundle.js',
       },
       module: {
